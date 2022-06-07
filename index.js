9d5fb7d2fa3dc4c7d559b674abf7d7f21d8c0282
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-07 07:46:35 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-let num1 ;
-let num2 ;
-let multiply ;
-multiply =num1 *num2;
-multiply = 62 ;
+//Multiply Function
+const num1 = 31;
+const num2 =  2;
+const num3 = 24;
+const num4 = 5;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4;
+let max =Math.max(1, 6, 8, 20);
